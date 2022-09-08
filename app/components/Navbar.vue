5ae1bcffb71de0f6fbfdd86d111ff6d8816e7058
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-09-08 12:41:50 +0200

    Improved SEO, improved mobile view, regenerated page

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -3,8 +3,8 @@ import Logo from '~/assets/branding/Sidebase_logo_dark_icon_font_for_dark_bg.svg
 </script>
 
 <template>
-  <div class="w-full flex items-center justify-between container mx-auto px-4 pt-2">
-    <div class="w-[200px] h-[50px] lg:w-[250px] lg:h-[100px] overflow-hidden flex items-center justify-center">
+  <div class="w-full flex items-center justify-between container mx-auto px-6 pt-2">
+    <div class="-ml-2 w-[150px] h-[50px] lg:w-[250px] lg:h-[100px] overflow-hidden flex items-center justify-center">
       <Logo />
     </div>
     <div class="flex items-center space-x-5">
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,6 +5,31 @@ import FeatureSection from '~/components/FeatureSection.vue'
 // import TestimonialsSection from '~/components/TestimonialsSection.vue'
 import ContactSection from '~/components/ContactSection.vue'
 import Footer from '~/components/Footer.vue'
+import { useHead } from '#head'
+
+const SITE_TITLE = 'SideBase ▸ by SideStream'
+const PREVIEW_IMAGE = '/Social_Preview.jpeg'
+const SITE_DESCRIPTION = 'sidebase is a modern, best-practice, batteries-included fullstack-app starter based on Nuxt3 and TypeScript.'
+const TWITTER_HANDLE = '@sidebase_io'
+
+useHead({
+  title: SITE_TITLE,
+  viewport: 'width=device-width, initial-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: SITE_DESCRIPTION },
+    { property: 'og:title', content: SITE_TITLE },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:description', content: SITE_DESCRIPTION },
+    { property: 'og:image', content: PREVIEW_IMAGE },
+    { name: 'theme-color', content: '#5bf3c7' },
+    { name: 'twitter:card', content: 'app' },
+    { name: 'twitter:site', content: TWITTER_HANDLE },
+    { name: 'twitter:title', content: SITE_TITLE },
+    { name: 'twitter:description', content: SITE_DESCRIPTION },
+    { name: 'twitter:image', content: PREVIEW_IMAGE },
+  ],
+})
 </script>
 
 <template>
