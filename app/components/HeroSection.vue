bc43a3e1e7a9a68048468fd15e2f1e2ebee3755c
Zoey <zoeykaiser8@gmail.com>
2022-09-08 12:42:47 +0200

    Improved SEO, improved mobile view, regenerated page (#4)

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex items-center justify-center h-[55vh] mx-4">
+  <div class="flex items-center justify-center h-[55vh] mx-4 md:mt-0">
     <div class="flex flex-col">
       <div class="text-center">
-        <img src="/Logo.png" class="h-[250px] w-[250px] mx-auto">
+        <img src="/Logo.png" class="h-[150px] w-[150px] md:h-[250px] md:w-[250px] mx-auto select-none">
         <h1 class="text-6xl md:text-8xl text-SideBase-green font-bold">
           sidebase
         </h1>
@@ -14,7 +14,7 @@
           <a href="https://github.com/sidestream-tech/sidebase" target="_blank">
             <Button label="Star on Github" class="p-button-outlined" icon="pi pi-star" icon-pos="left" />
           </a>
-          <a href="https://github.com/sidestream-tech/sidebase/generate" target="_blank">
+          <a class="hidden md:block" href="https://github.com/sidestream-tech/sidebase/generate" target="_blank">
             <Button label="Use starter template" icon="pi pi-flag" icon-pos="left" />
           </a>
         </div>
