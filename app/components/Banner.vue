8a84a3303edfa38962afb2f89a56ad2ef65c1475
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-09-09 18:06:44 +0200

    Added Banner

---
+++ app/components/Banner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full py-4 shadow bg-gray-800">
+    <div class="container mx-auto flex items-center justify-between">
+      <div class="flex items-center gap-3 text-gray-300">
+        <i class="pi pi-bolt" style="font-size: 2rem; margin-top: 2px" />
+        <div>
+          <h1 class="text-lg font-bold">
+            We just released our package <span class="font-mono bg-gray-700 p-1 rounded-lg">nuxt-sidebase-parse</span>
+          </h1>
+          <p class="text-xs">
+            A DX focused Nuxt3 package that simplifies data validation and parsing!
+          </p>
+        </div>
+      </div>
+      <a href="https://nuxt-sidebase-parse.sidebase.io/index.html" target="_blank">
+        <Button label="View documentation" class="p-button-sm" icon="pi pi-book" icon-pos="left" />
+      </a>
+    </div>
+  </div>
+</template>
+
